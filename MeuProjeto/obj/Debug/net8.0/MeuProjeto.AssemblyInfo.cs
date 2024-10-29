6eeb1045465acdccc1655805b1ef6ffa7d2f7248
Breno Santos <breno.dev27@gmail.com>
2024-10-29 18:21:50 -0300

    unit-tests

--- MeuProjeto/obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
+++ MeuProjeto/obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25ec7aa12d1c12f66004cb3fb24d8e34f58ac90")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
